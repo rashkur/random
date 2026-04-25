11eac0ca6f68c5d385ac4e5bd7372e71d812af8f
Roman S <rashkur@gmail.com>
2026-04-25 13:53:52 +0200

    Delete snap_root.bash

--- snap_root.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export AWS_DEFAULT_REGION='us-east-2'
-export AWS_ACCESS_KEY_ID='AKIAxxxxxxxxxxxx'
-export AWS_SECRET_ACCESS_KEY='xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-
-## instancexxx
-DESC=$(date +instancexxx-root-%d-%m-%Y)
-aws ec2 create-snapshot --volume-id vol-0101010101 --description "$DESC"
